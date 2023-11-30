dff55b81ee59e1dbfc526eda3ead424fbe8c44b6
Md. Siam Biswas <siam.biswas@icloud.com>
2023-11-29 22:29:02 -0800

    Initial Commit

---
+++ SwiftUIEssentials/SwiftUIEssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEssentialsApp.swift
+//  SwiftUIEssentials
+//
+//  Created by Siam Biswas on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
